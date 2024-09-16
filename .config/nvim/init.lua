11ec0746dd28d5181e2deb501af5c2bcc57fa807
phandinhthe <phandinhthe1991@gmail.com>
2024-09-16 08:39:16 +0700

    Delete all

--- .config/nvim/init.lua
+++
@@ -1,8 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-
-vim.cmd("highlight Normal guibg=none")
-vim.cmd("highlight NonText guibg=none")
-vim.cmd("highlight Normal ctermbg=none")
-vim.cmd("highlight NonText ctermbg=none")
-vim.cmd("set scrolloff=100")
--- .config/nvim/lua/config/options.lua
+++
@@ -1,8 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
---
-vim.wo.number = true -- set true for windows options number.
-vim.api.nvim_set_option("number", true) -- set true for windows options number.
-
-vim.opt.cursorline = false -- turn off the highlight cursorline.
